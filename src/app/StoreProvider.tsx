23c3fc029c9407cadfc0193a1a827a80117d99a7
fredrekt <fredrickjohng6@gmail.com>
2024-02-03 22:40:44 +0800

    added persistent state

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,11 +1,24 @@
 'use client';
 import { Provider } from 'react-redux';
 import store from '@dir/utils/store';
+import { useEffect } from 'react';
+import { setState } from '@dir/utils/breedSlice';
 
-interface StoreProvider {
+interface StoreProviderProps {
 	children: React.ReactNode;
 }
 
-export default function StoreProvider({ children }: StoreProvider) {
+const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {
+	// mock persistent state workaround
+	useEffect(() => {
+		const savedState = typeof window !== 'undefined' && localStorage.getItem('breedState');
+		if (savedState) {
+			const parsedState = JSON.parse(savedState);
+			store.dispatch(setState(parsedState));
+		}
+	}, []);
+
 	return <Provider store={store}>{children}</Provider>;
-}
+};
+
+export default StoreProvider;
