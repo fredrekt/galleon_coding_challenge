33fcfa013861f5575ce0cf46bff6da7ceab2127b
fredrekt <fredrickjohng6@gmail.com>
2024-02-03 17:53:42 +0800

    added font theme for mui

---
+++ src/utils/theme.ts
@@ -0,0 +1,18 @@
+// src/theme.ts
+'use client';
+import { createTheme } from '@mui/material/styles';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+	weight: ['300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+	display: 'swap'
+});
+
+const theme = createTheme({
+	typography: {
+		fontFamily: montserrat.style.fontFamily
+	}
+});
+
+export default theme;
