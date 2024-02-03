37e343b3239c763b630455219471ddec83c8bf0c
fredrekt <fredrickjohng6@gmail.com>
2024-02-03 18:00:11 +0800

    init dog page slug

---
+++ src/app/breed/[breedName]/page.tsx
@@ -0,0 +1,24 @@
+import { getByBreed } from '@component/api/api';
+import DogCard from '@component/components/DogCard/DogCard';
+import React from 'react';
+
+const BreedSlugpage = async ({ params }: { params: { breedName: string } }) => {
+	const { message } = await getByBreed(params.breedName);
+
+	const renderBreedImages = () => {
+		if (!Array.isArray(message) || !message.length) {
+			return;
+		}
+		return (
+			<>
+				{message.map((imgUrl) => (
+					<DogCard imageUrl={imgUrl} />
+				))}
+			</>
+		);
+	};
+
+	return <div>{renderBreedImages()}</div>;
+};
+
+export default BreedSlugpage;
